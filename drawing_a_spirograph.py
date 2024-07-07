b7e52fe15ecf81f1acf2363c4688d512376b10a1
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-07-07 04:00:12 -0500

    Update drawing_a_spirograph.py

--- drawing_a_spirograph.py
+++ drawing_a_spirograph.py
@@ -10,6 +10,8 @@ def random_color():
     color = (r, g, b)
     return color
 
+tim.speed("fastest")
+
 def draw_spirograph(size_of_gap):
     for _ in range(int(360 / size_of_gap)):
         tim.color(random_color())
